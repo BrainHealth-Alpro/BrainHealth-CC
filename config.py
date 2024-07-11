31ac38a99dfa52f7ffa55f1e03427ae27015914b
gmaaliki <maalikighifari@gmail.com>
2024-07-11 10:15:10 +0700

    refactor: moving env variable to config

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ import os
 class Config:
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
     MAX_CONTENT_LENGTH = 1024 * 1024 * 1024 # 1 GB
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/brainhealth'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
 
 class DevelopmentConfig(Config):
     DEBUG = True
