37ef52e8bd9896900217a610953e6cec1b71288a
gmaaliki <maalikighifari@gmail.com>
2024-07-11 10:10:25 +0700

    refactor: changing GET /api/csrf to API format

---
+++ apis/history/routes.py
@@ -0,0 +1,35 @@
+from flask import jsonify, request
+from flask_restx import Namespace, Resource, abort
+from apis.profile import profile
+from models import User, db
+
+ns = Namespace('api', description='Manage profile')
+
+@ns.route('/profile')
+class Predict(Resource):
+    @ns.doc('post_profile')
+    def post(self):
+        args = request.get_json()
+
+        if not args:
+            abort(400, 'Invalid JSON')
+
+        required_fields = ['id', 'nama_lengkap', 'email', 'nomor_telepon', 'foto_profil', 'tempat_lahir',
+                           'tanggal_lahir', 'kata_sandi', 'tipe']
+        for field in required_fields:
+            if field not in args:
+                return abort(400, 'Missing required field')
+
+        profile.make_form(args)
+        return profile.post_profile()
+
+
+    @ns.doc('get_profile')
+    def get(self):
+        user_id = request.args.get('user_id', '')
+        user = db.session.query(User).get(user_id)
+
+        if not user:
+            return abort(404, 'User not found.')
+
+        return profile.get_profile(user)
---
+++ apis/history/utility.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from flask import jsonify, abort
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email, EqualTo
+from models import User, db
+import os
+
+class ProfileForm(FlaskForm):
+    id = IntegerField('ID', validators=[DataRequired()])
+    nama_lengkap = StringField('Nama Lengkap', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    nomor_telepon = StringField('Nomor Telepon', validators=[DataRequired()])
+    foto_profil = StringField('Foto Profil', validators=[DataRequired()])
+    tempat_lahir = StringField('Tempat Lahir', validators=[DataRequired()])
+    tanggal_lahir = StringField('Tanggal Lahir', validators=[DataRequired()])
+    kata_sandi = PasswordField('Kata Sandi', validators=[DataRequired(), EqualTo('kata_sandi')])
+    tipe = StringField('Tipe', validators=[DataRequired()])
+    submit = SubmitField('Update Profile')
+
+class Profile:
+    def __init__(self):
+        self.form = None
+
+    def post_profile(self):
+        if self.form.validate():
+            user = User.query.get(self.form.id.data)
+            if not user:
+                return jsonify({'message': 'User not found.'}), 404
+
+            user.nama_lengkap = self.form.nama_lengkap.data
+            user.email = self.form.email.data
+            user.nomor_telepon = self.form.nomor_telepon.data
+            user.foto_profil = self.form.foto_profil.data
+            user.tempat_lahir = self.form.tempat_lahir.data
+            user.tanggal_lahir = self.form.tanggal_lahir.data
+            user.kata_sandi = self.form.kata_sandi.data
+            user.tipe = self.form.tipe.data
+
+            db.session.commit()
+
+            return jsonify({'message': 'Profile updated successfully.'})
+        else:
+            errors = []
+            for field, errors_list in self.form.errors.items():
+                for error in errors_list:
+                    errors.append(f'{field}: {error}')
+            # Gabisa nge return errornya, bingung kyk gimana
+            abort (400, 'Validation failed')
+            # return jsonify({'message': 'Validation failed', 'errors': errors}), 400
+
+    def get_profile(self, user):
+        return jsonify({
+            'id': user.id,
+            'nama_lengkap': user.nama_lengkap,
+            'email': user.email,
+            'nomor_telepon': user.nomor_telepon,
+            'foto_profil': user.foto_profil,
+            'tempat_lahir': user.tempat_lahir,
+            'tanggal_lahir': user.tanggal_lahir,
+            'kata_sandi': user.kata_sandi,
+            'tipe': user.tipe,
+        })
+
+    def make_form(self, data):
+        self.form = ProfileForm(data=data)
