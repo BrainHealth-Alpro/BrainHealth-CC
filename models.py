f9cb8a77f6eff547b97482720078a1bb03aaf1bc
richiesuper <richie9963@gmail.com>
2024-07-11 19:39:23 +0700

    feat: add Gambar model

--- models.py
+++ models.py
@@ -7,12 +7,22 @@ def init_db(app):
     db.init_app(app)
     return db
 
+class Gambar(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.Text, nullable=False)
+    user = db.relationship('User', backref='gambar', lazy=True)
+    riwayat = db.relationship('Riwayat', backref='riwayat', lazy=True)
+
+    def __repr__(self):
+        return '<Gambar id=%r path=%r>' % (self.id, self.path)
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nama_lengkap = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     nomor_telepon = db.Column(db.String(20), unique=True, nullable=False)
-    foto_profil = db.Column(db.Text, nullable=True)
+    gambar_id = db.Column(db.Integer, db.ForeignKey('gambar.id'), nullable=True)
     tempat_lahir = db.Column(db.Text, nullable=True)
     tanggal_lahir = db.Column(db.Date, nullable=True)
     kata_sandi = db.Column(db.Text, nullable=False)
@@ -20,7 +30,7 @@ class User(db.Model):
     riwayat = db.relationship('Riwayat', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User id=%r nama_lengkap=%r email=%r nomor_telepon=%r foto_profil=%r tempat_lahir=%r tanggal_lahir=%r kata_sandi=%r tipe=%r>' % (self.id, self.nama_lengkap, self.email, self.nomor_telepon, self.foto_profil, self.tempat_lahir, self.tanggal_lahir, self.kata_sandi, self.tipe)
+        return '<User id=%r nama_lengkap=%r email=%r nomor_telepon=%r gambar_id=%r tempat_lahir=%r tanggal_lahir=%r kata_sandi=%r tipe=%r>' % (self.id, self.nama_lengkap, self.email, self.nomor_telepon, self.gambar_id, self.tempat_lahir, self.tanggal_lahir, self.kata_sandi, self.tipe)
     
     def set_password(self, kata_sandi):
         self.kata_sandi = generate_password_hash(kata_sandi)
@@ -44,9 +54,9 @@ class Riwayat(db.Model):
     nama_lengkap_pasien = db.Column(db.String(80), nullable=False)
     hasil = db.Column(db.String(30), nullable=False) # Tidak ada tumor | Terdapat tumor terdeteksi
     datetime = db.Column(db.DateTime, nullable=False)
-    gambar = db.Column(db.Text, nullable=False) # bisa hyperlink atau internal path ke file
+    gambar_id = db.Column(db.Integer, db.ForeignKey('gambar.id'), nullable=False)
     tumor_id = db.Column(db.Integer, db.ForeignKey('tumor.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return '<Riwayat id=%r nama_lengkap_pasien=%r hasil=%r tanggal=%r waktu=%r gambar=%r tumor_id=%r user_id=%r>' % (self.id, self.nama_lengkap_pasien, self.hasil, self.tanggal, self.waktu, self.gambar, self.tumor_id, self.user_id)
+        return '<Riwayat id=%r nama_lengkap_pasien=%r hasil=%r datetime=%r gambar_id=%r tumor_id=%r user_id=%r>' % (self.id, self.nama_lengkap_pasien, self.hasil, self.datetime, self.gambar_id, self.tumor_id, self.user_id)
