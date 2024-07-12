8da4cab32a9bc2f29fef015ac2a6524d1257edd4
richiesuper <richie9963@gmail.com>
2024-07-12 14:36:20 +0700

    fix: save profile photos to correct directory

--- apis/profile_photo/utility.py
+++ apis/profile_photo/utility.py
@@ -7,11 +7,11 @@ from models import User, db
 import os
 
 class ProfilePhoto:
-    def _save_image(self, file, ext, upload_dir=current_app.config['PROFILE_PHOTO_FOLDER'], upload_name=''):
+    def _save_image(self, file, ext, upload_dir=current_app.config['UPLOAD_FOLDER'], upload_name=''):
         if upload_name == '':
-            filepath = os.path.join(upload_dir, datetime.now().strftime('%Y%m%d%H%M%S') + ext)
+            filepath = os.path.join(upload_dir + '/profile_photos', datetime.now().strftime('%Y%m%d%H%M%S') + ext)
         else:
-            filepath = os.path.join(upload_dir, upload_name + ext)
+            filepath = os.path.join(upload_dir + '/profile_photos', upload_name + ext)
         
         file.save(filepath)
         return filepath
