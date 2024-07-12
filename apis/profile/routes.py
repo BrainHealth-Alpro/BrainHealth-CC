60a19f055e627d5d15aeacb7e425c6bae3010d22
richiesuper <richie9963@gmail.com>
2024-07-12 16:44:03 +0700

    buggy profile

--- apis/profile/routes.py
+++ apis/profile/routes.py
@@ -1,27 +1,40 @@
-from flask import jsonify, request
+from flask import jsonify, request, current_app
 from flask_restx import Namespace, Resource, abort
 from apis.profile import profile
 from models import User, db
+from werkzeug.datastructures import FileStorage
 
 ns = Namespace('api', description='Manage profile')
+
+parser = ns.parser()
+parser.add_argument('id', location='form', type=int, required=True, help='id')
+parser.add_argument('nama_lengkap', location='form', type=str, required=False, help='nama_lengkap')
+parser.add_argument('email', location='form', type=str, required=False, help='email')
+parser.add_argument('nomor_telepon', location='form', type=str, required=False, help='nomor_telepon')
+parser.add_argument('gambar', location='files', type=FileStorage, required=False, help='gambar')
+parser.add_argument('tempat_lahir', location='form', type=str, required=False, help='tempat_lahir')
+parser.add_argument('tanggal_lahir', location='form', type=str, required=False, help='tanggal_lahir')
+parser.add_argument('kata_sandi', location='form', type=str, required=False, help='kata_sandi')
+parser.add_argument('tipe', location='form', type=str, required=False, help='tipe')
 
 @ns.route('/profile')
 class ProfileRoute(Resource):
     @ns.doc('post_profile')
+    @ns.expect(parser)
     def post(self):
-        args = request.get_json()
+        args = parser.parse_args()
+        id = args['id']
+        nama_lengkap = args['nama_lengkap']
+        email = args['email']
+        nomor_telepon = args['nomor_telepon']
+        gambar = args['gambar']
+        filename = gambar.filename
+        tempat_lahir = args['tempat_lahir']
+        tanggal_lahir = args['tanggal_lahir']
+        kata_sandi = args['kata_sandi']
+        tipe = args['tipe']
 
-        if not args:
-            abort(400, 'Invalid JSON')
-
-        required_fields = ['id', 'nama_lengkap', 'email', 'nomor_telepon', 'gambar_id', 'tempat_lahir',
-                           'tanggal_lahir', 'kata_sandi', 'tipe']
-        for field in required_fields:
-            if field not in args:
-                return abort(400, 'Missing required field')
-
-        profile.make_form(args)
-        return profile.post_profile()
+        return profile.post_profile(id=id, nama_lengkap=nama_lengkap, email=email, nomor_telepon=nomor_telepon, gambar=gambar, filename=filename, tempat_lahir=tempat_lahir, tanggal_lahir=tanggal_lahir, kata_sandi=kata_sandi, tipe=tipe, upload_dir=current_app.config['UPLOAD_FOLDER'])
 
 
     @ns.doc('get_profile')
--- apis/profile/utility.py
+++ apis/profile/utility.py
@@ -1,53 +1,50 @@
 from datetime import datetime
-from flask import jsonify, abort
+from flask import jsonify, abort, current_app
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Email, EqualTo
 from models import User, Gambar, db
 import os
 
-class ProfileForm(FlaskForm):
-    id = IntegerField('ID', validators=[DataRequired()])
-    nama_lengkap = StringField('Nama Lengkap', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    nomor_telepon = StringField('Nomor Telepon', validators=[DataRequired()])
-    gambar_id = IntegerField('Gambar ID', validators=[DataRequired()])
-    tempat_lahir = StringField('Tempat Lahir', validators=[DataRequired()])
-    tanggal_lahir = StringField('Tanggal Lahir', validators=[DataRequired()])
-    kata_sandi = PasswordField('Kata Sandi', validators=[DataRequired(), EqualTo('kata_sandi')])
-    tipe = StringField('Tipe', validators=[DataRequired()])
-    submit = SubmitField('Update Profile')
-
 class Profile:
     def __init__(self):
         self.form = None
-
-    def post_profile(self):
-        if self.form.validate():
-            user = User.query.get(self.form.id.data)
-            if not user:
-                return jsonify({'message': 'User not found.'}), 404
-
-            user.nama_lengkap = self.form.nama_lengkap.data
-            user.email = self.form.email.data
-            user.nomor_telepon = self.form.nomor_telepon.data
-            user.gambar_id = self.form.gambar_id.data
-            user.tempat_lahir = self.form.tempat_lahir.data
-            user.tanggal_lahir = self.form.tanggal_lahir.data
-            user.kata_sandi = self.form.kata_sandi.data
-            user.tipe = self.form.tipe.data
-
-            db.session.commit()
-
-            return jsonify({'message': 'Profile updated successfully.'}), 200
+        
+    def _save_image(self, file, ext, upload_dir, upload_name=''):
+        if upload_name == '':
+            filepath = os.path.join(upload_dir + '/profile_photos', datetime.now().strftime('%Y%m%d%H%M%S') + ext)
         else:
-            errors = []
-            for field, errors_list in self.form.errors.items():
-                for error in errors_list:
-                    errors.append(f'{field}: {error}')
-            # Gabisa nge return errornya, bingung kyk gimana
-            abort (400, 'Validation failed')
-            # return jsonify({'message': 'Validation failed', 'errors': errors}), 400
+            filepath = os.path.join(upload_dir + '/profile_photos', upload_name + ext)
+        
+        file.save(filepath)
+        return filepath
+        
+
+    def post_profile(self, id, nama_lengkap, email, nomor_telepon, gambar, filename, tempat_lahir, tanggal_lahir, kata_sandi, tipe, upload_dir):
+        user = User.query.get(id)
+        if not user:
+            return abort(404, "User not found.")
+
+        user.nama_lengkap = nama_lengkap
+        user.email = email
+        user.nomor_telepon = nomor_telepon
+        
+        # impement file saving logic
+        path = self._save_image(gambar, os.path.splitext(filename)[1], upload_dir=upload_dir)
+        
+        new_gambar = Gambar(path=path)
+        db.session.add(new_gambar)
+        db.session.commit()
+
+        user.gambar_id = new_gambar.id
+        user.tempat_lahir = tempat_lahir
+        user.tanggal_lahir = tanggal_lahir
+        user.kata_sandi = kata_sandi
+        user.tipe = tipe
+
+        db.session.commit()
+
+        return jsonify({'message': 'Profile updated successfully.'})
 
     def get_profile(self, user):
         foto_profil_path = db.query(Gambar).get(user.gambar_id)
