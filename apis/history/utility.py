05732725ab725d4bf6a8f6600bf5069d9d176e1b
richiesuper <richie9963@gmail.com>
2024-07-12 12:44:32 +0700

    feat: change history GET to return gambar_path and history POST to accept gambar_id

--- apis/history/utility.py
+++ apis/history/utility.py
@@ -3,14 +3,14 @@ from flask import jsonify, abort
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, DateTimeField
 from wtforms.validators import DataRequired, Email, EqualTo
-from models import User, Riwayat, Tumor, db
+from models import User, Riwayat, Tumor, Gambar, db
 import os
 
 class HistoryForm(FlaskForm):
     nama_lengkap_pasien = StringField('Nama Lengkap Pasien', validators=[DataRequired()])
     hasil = StringField('Hasil', validators=[DataRequired()])
     datetime = DateTimeField('Datetime', validators=[DataRequired()])
-    gambar = StringField('Gambar', validators=[DataRequired()])
+    gambar_id = IntegerField('Gambar ID', validators=[DataRequired()])
     tumor_id = IntegerField('Tumor ID', validators=[DataRequired()])
     user_id = IntegerField('User ID', validators=[DataRequired()])
     submit = SubmitField('Catat History')
@@ -24,13 +24,13 @@ class History:
             nama_lengkap_pasien = self.form.nama_lengkap_pasien.data
             hasil = self.form.hasil.data
             datetime = self.form.datetime.data
-            gambar = self.form.gambar.data
+            gambar_id = self.form.gambar_id.data
             tumor_id = self.form.tumor_id.data
             user_id = self.form.user_id.data
 
             # Create a new history
             new_history = Riwayat(nama_lengkap_pasien=nama_lengkap_pasien, hasil=hasil, datetime=datetime,
-                                  gambar=gambar, tumor_id=tumor_id, user_id=user_id)
+                                  gambar_id=gambar_id, tumor_id=tumor_id, user_id=user_id)
             db.session.add(new_history)
             db.session.commit()
 
@@ -43,11 +43,11 @@ class History:
             return jsonify({'message': 'Validation failed', 'errors': errors})
 
     def get_history(self, user):
-        history = db.session.query(Riwayat, Tumor).join(Tumor).filter(Riwayat.tumor_id == Tumor.id).all()
+        history = db.session.query(Riwayat, Tumor).join(Tumor).filter(Riwayat.tumor_id == Tumor.id).join(Gambar).filter(Riwayat.gambar_id == Gambar.id).all()
 
         history_list = [{'id': riwayat.id, 'nama_lengkap_pasien': riwayat.nama_lengkap_pasien,
                         'hasil': riwayat.hasil, 'datetime': riwayat.datetime,
-                        'gambar': riwayat.gambar, 'jenis_tumor': tumor.nama, 'user_id': riwayat.user_id } for riwayat, tumor in history]
+                        'gambar_path': gambar.path, 'jenis_tumor': tumor.nama, 'user_id': riwayat.user_id } for riwayat, tumor, gambar in history]
 
         return jsonify({'history': history_list})
 
